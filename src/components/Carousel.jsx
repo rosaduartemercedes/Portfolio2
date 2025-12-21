ce5bcbdea4343d4436994daa65b590bbb57715f5
VickyDuarte1 <rosa.duarte.mercedes@gmail.com>
2025-12-21 01:11:01 -0300

    link carrousel corregido

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,7 +8,7 @@ export default function Carousel() {
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    fetch("https://portfolio-look-productions.vercel.app/api/carrousell")
+    fetch("https://mi-galeria-back.vercel.app/api/carrousell")
       .then((res) => res.json())
       .then((data) => setImages(data));
   }, []);
